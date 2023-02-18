c9b223d711222c8df27d27678b4d4d38ab3f448b
Sanjib Sah <sanjib@indiagold.co>
2023-02-19 02:30:37 +0530

    deleted src in lexical-editor

--- packages/lexical-editor/src/plugins/LinkPlugin/index.tsx
+++
@@ -1,9 +0,0 @@
-import { LinkPlugin as LexicalLinkPlugin } from "@lexical/react/LexicalLinkPlugin";
-
-import { validateUrl } from "lib/url";
-
-export default function LinkPlugin(): JSX.Element {
-  return <LexicalLinkPlugin validateUrl={validateUrl} />;
-}
-
-export { LinkPlugin };
