a2c1c078f796e8ea1b67fadbb1ec16a06d623623
Sanjib Sah <sanjib@indiagold.co>
2023-02-19 02:54:15 +0530

    Removed copyright message

--- packages/lexical-editor/plugins/TestRecorderPlugin/index.tsx
+++ packages/lexical-editor/plugins/TestRecorderPlugin/index.tsx
@@ -177,7 +177,7 @@ function useTestRecorder(
 
     return `
 /**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) BlogX.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
