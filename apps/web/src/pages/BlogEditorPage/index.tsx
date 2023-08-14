8d8f662d3f3153a82e7a7536e37e77a1c03bdc82
Sanjib <sanjib.sah@yahoo.com>
2023-08-14 19:05:54 +0530

    minor changes

--- apps/web/src/pages/BlogEditorPage/index.tsx
+++ apps/web/src/pages/BlogEditorPage/index.tsx
@@ -1,12 +1,14 @@
+import "./index.css";
 import { Editor } from "@blogx/lexical-editor/Editor";
 import { BlogHeader } from "ui";
 
 const BlogEditorPage = () => {
-  return <div className="editor-shell">
-    <BlogHeader />
-    <Editor />
-  </div>
-
+  return (
+    <div className="editor-shell">
+      <BlogHeader />
+      <Editor />
+    </div>
+  );
 };
 
 export default BlogEditorPage;
