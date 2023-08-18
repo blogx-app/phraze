0aec46dfed901c7c888d34ef915b6de4324464d0
Sanjib <sanjib.sah@yahoo.com>
2023-08-18 21:15:19 +0530

    added sidebar

--- packages/ui/Sidebar/index.tsx
+++ packages/ui/Sidebar/index.tsx
@@ -4,27 +4,31 @@ import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+// import Divider from "@mui/material/Divider";
+// import IconButton from "@mui/material/IconButton";
+// import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+// import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import AmongUsIcon from "./icons/AmongUsIcon";
+import UserIcon from "./icons/UserIcon";
+import SunIcon from "./icons/SunIcon";
+import { BottomIconsContainer } from "./style";
 
 const drawerWidth = 240;
 
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
+// const openedMixin = (theme: Theme): CSSObject => ({
+//   width: drawerWidth,
+//   transition: theme.transitions.create("width", {
+//     easing: theme.transitions.easing.sharp,
+//     duration: theme.transitions.duration.enteringScreen,
+//   }),
+//   overflowX: "hidden",
+// });
 
 const closedMixin = (theme: Theme): CSSObject => ({
   transition: theme.transitions.create("width", {
@@ -33,31 +37,30 @@ const closedMixin = (theme: Theme): CSSObject => ({
   }),
   overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
+  borderRadius: "12px",
+  height: "calc(100vh - 2rem)",
+  background: "black",
+  marginLeft: "1rem",
+  marginTop: "1rem",
   [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
 
 const Drawer = styled(MuiDrawer, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   width: drawerWidth,
   flexShrink: 0,
+  background: "transparent",
+  borderRadius: "8px",
   whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
+  marginTop: "1rem",
+  // boxSizing: "border-box",
+  // ...(open && {
+  //   ...openedMixin(theme),
+  //   "& .MuiDrawer-paper": openedMixin(theme),
+  // }),
   ...(!open && {
     ...closedMixin(theme),
     "& .MuiDrawer-paper": closedMixin(theme),
@@ -65,27 +68,24 @@ const Drawer = styled(MuiDrawer, {
 }));
 
 interface MiniDrawerProps {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 export default function MiniDrawer({ children }: MiniDrawerProps) {
   const [open, setOpen] = React.useState(false);
 
-  const handleDrawerClose = () => {
-    setOpen((state) => !state);
-  };
+  // const handleDrawerClose = () => {
+  //   setOpen((state) => !state);
+  // };
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {!open ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
+        <div style={{ margin: "0 auto", marginTop: "0.5rem" }}>
+          <AmongUsIcon />
+        </div>
+        <List style={{ margin: "auto" }}>
           {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
             <ListItem key={text} disablePadding sx={{ display: "block" }}>
               <ListItemButton
@@ -102,13 +102,21 @@ export default function MiniDrawer({ children }: MiniDrawerProps) {
                     justifyContent: "center",
                   }}
                 >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {index % 2 === 0 ? (
+                    <InboxIcon color="error" />
+                  ) : (
+                    <MailIcon color="error" />
+                  )}
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
+        <BottomIconsContainer>
+          <SunIcon />
+          <UserIcon />
+        </BottomIconsContainer>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {children}
---
+++ packages/ui/Sidebar/style.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const BottomIconsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+
+  margin: 0.5rem auto;
+  margin-bottom: 1.5rem;
+`;
