78cfb3a3be5d5fce65265cbfbd92209f80a5f3da
Sanjib <sanjib.sah@yahoo.com>
2023-10-18 00:43:54 +0530

    remove editor-server

--- packages/lexical-editor/collaboration.ts
+++ packages/lexical-editor/collaboration.ts
@@ -1,38 +1,15 @@
 import { Provider } from "@lexical/yjs";
-import { TiptapCollabProvider, HocuspocusProvider } from "@hocuspocus/provider";
-// import { WebsocketProvider } from "y-websocket";
+import { HocuspocusProvider } from "@hocuspocus/provider";
+
 import * as Y from "yjs";
 
-const url = new URL(window.location.href);
-const params = new URLSearchParams(url.search);
-const WEBSOCKET_ENDPOINT =
-  params.get("collabEndpoint") || "ws://localhost:8080";
-const WEBSOCKET_SLUG = "collaboration";
-const WEBSOCKET_ID = params.get("collabId") || "0";
-
-// parent dom -> child doc
 export function createWebsocketProvider(
   id: string,
   yjsDocMap: Map<string, Y.Doc>
 ): Provider {
-  // let doc = yjsDocMap.get(id);
-
-  // if (doc === undefined) {
-  //   doc = new Doc();
-  //   yjsDocMap.set(id, doc);
-  // } else {
-  //   doc.load();
-  // }
-
-  // // @ts-ignore
-  // return new WebsocketProvider(WEBSOCKET_ENDPOINT, WEBSOCKET_SLUG, doc, {
-  //   connect: false,
-  // });
-
   const doc = new Y.Doc();
   yjsDocMap.set(id, doc);
 
-  // @TODO: REPLACE APP ID
   // @TODO: PUT PROPER TOKEN
   // @TODO: OR USE `HocuspocusProvider` with Hocuspocus URL
   const hocuspocusProvider = new HocuspocusProvider({
