38eb97c9ec97e707ba68dada8bf6c3ef8210ecf2
Sanjib <sanjib.sah@yahoo.com>
2023-10-26 18:26:26 +0530

    removed sidebar and added top nav to editorpage

--- apps/web/src/pages/BlogEditorPage/index.tsx
+++ apps/web/src/pages/BlogEditorPage/index.tsx
@@ -1,13 +1,49 @@
 import "./index.css";
+import { useTheme } from "@emotion/react";
 import { Editor } from "@blogx/lexical-editor/Editor";
-import { BlogHeader } from "@phraze-app/ui";
+import { BlogHeader, Flex, StateTags } from "@phraze-app/ui";
+import { ChevronLeft } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { routesName } from "../../route";
+import BlogSettingsIcon from "./icons/BlogSettingsIcon";
 
 const BlogEditorPage = () => {
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  const onClickBack = () => {
+    navigate(routesName.posts);
+  };
+
   return (
-    <div className="editor-shell">
-      <BlogHeader />
-      <Editor />
-    </div>
+    <Flex flexDirection="column">
+      <Flex
+        color={theme.colors.textWhite}
+        justifyContent="space-between"
+        alignItems="center"
+        width="95%"
+        margin="0 auto"
+      >
+        <Flex gap="1rem">
+          <span
+            style={{ cursor: "pointer", display: "flex" }}
+            onClick={onClickBack}
+          >
+            <ChevronLeft /> Posts
+          </span>
+          <StateTags type="draft" tagType="outlined">
+            Draft
+          </StateTags>
+        </Flex>
+        <span style={{ cursor: "pointer" }}>
+          <BlogSettingsIcon />
+        </span>
+      </Flex>
+      <div className="editor-shell">
+        <BlogHeader />
+        <Editor />
+      </div>
+    </Flex>
   );
 };
 
