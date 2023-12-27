f5cd3029fd5cdf0cb7389a269073ada0498bd633
Sanjib <sanjib.sah@yahoo.com>
2023-12-27 13:01:43 +0530

    minor changes

--- apps/web/src/pages/BlogEditorPage/index.tsx
+++ apps/web/src/pages/BlogEditorPage/index.tsx
@@ -11,10 +11,11 @@ import {
 } from "@phraze-app/ui";
 import { ChevronLeft } from "@mui/icons-material";
 import { routesName } from "../../route";
-import BlogSettingsIcon from "./icons/BlogSettingsIcon";
 import useAppNavigation from "../../hooks/useAppNavigation";
+import { useState } from "react";
 
 const BlogEditorPage = () => {
+  const [openSettingsDrawer, setOpenSettingsDrawer] = useState(false);
   const theme = useTheme();
   const navigate = useAppNavigation({
     blogName: "acme",
@@ -24,16 +25,23 @@ const BlogEditorPage = () => {
     navigate(routesName.posts);
   };
 
+  const onClickOpenDrawer = () => {
+    setOpenSettingsDrawer(true);
+  };
+
   return (
     <Flex flexDirection="column">
       <Flex
         color={theme.colors.textWhite}
         justifyContent="space-between"
         alignItems="center"
-        width="95%"
+        width="100%"
+        px={4}
+        py={2}
         margin="0 auto"
+        borderBottom={theme.borders.card}
       >
-        <Flex gap="1rem">
+        <Flex gap="1rem" alignItems="center">
           <span
             style={{ cursor: "pointer", display: "flex" }}
             onClick={onClickBack}
@@ -44,18 +52,19 @@ const BlogEditorPage = () => {
             Draft
           </StateTags>
         </Flex>
-        <span style={{ cursor: "pointer" }}>
-          <BlogSettingsIcon />
+        <span style={{ cursor: "pointer" }} onClick={onClickOpenDrawer}>
+          <div>Some Icon</div>
         </span>
       </Flex>
       <div className="editor-shell">
         <BlogHeader />
         <Editor />
-        <PhzDrawer open={true} anchor="right">
+        {/* Create this drawer using something from Radix. */}
+        {/* <PhzDrawer open={false} anchor="right">
           <Box p={1}>
             <Text>Riight Drawer</Text>
           </Box>
-        </PhzDrawer>
+        </PhzDrawer> */}
       </div>
     </Flex>
   );
